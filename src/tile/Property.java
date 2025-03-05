7624df8424f20554a43887d11df34346bffcd67e
Ryan <ryan.opitz@mycc.caspercollege.edu>
2025-03-04 17:09:25 -0700

    Update Property.java
    Getters and setters

--- src/tile/Property.java
+++ src/tile/Property.java
@@ -5,8 +5,15 @@ public class Property extends Tile{
     private String propertyOwner;
     private final int[] rent = {50, 200, 600, 1400, 1700, 2000};
     boolean setOwned;
-    public void setName(String tileName) {name = tileName;}
-    public String getName() {return name;}
+
+    // Get and set property name
+    public void setName(String tileName) { name = tileName; }
+    public String getName() { return name; }
+
+    // Get and set owner name
+    public void setOwner(String player) { propertyOwner = player; }
+    public String getOwner() { return propertyOwner; }
+
     public void landOn(){
         // Do stuff
     }
