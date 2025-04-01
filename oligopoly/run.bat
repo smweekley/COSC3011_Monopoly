aeece4205e0584556145257f95d729bd46074380
Worsleychase <worsleychase@yahoo.com>
2025-04-01 16:29:32 -0600

    Changed some stuff for running

--- oligopoly/run.bat
+++ oligopoly/run.bat
@@ -2,7 +2,7 @@
 
 if exist bin (rd /s /q bin)
 
-javac --module-path ".\lib\javafx\lib" --add-modules javafx.controls,javafx.fxml -d bin src/*.java src/board/*.java src/fxml/*.java src/player/*.java src/tile/*.java
+javac --module-path ".\lib\javafx\lib" --add-modules javafx.controls,javafx.fxml -d bin src/*.java src/board/*.java src/fxml/*.java src/player/*.java src/tile/*.java src/state/*.java
 java --module-path ".\lib\javafx\lib" --add-modules javafx.controls,javafx.fxml -cp bin Main
 
 pause
