66698454935dff6782b1b49daa1fca6b7064bcea
Worsleychase <worsleychase@yahoo.com>
2025-03-31 18:31:33 -0600

    Added check for bin to avoid error msg

--- oligopoly/run.bat
+++ oligopoly/run.bat
@@ -1,6 +1,8 @@
 @echo off
 
-rd /s /q bin
+if exist bin (rd /s /q bin)
 
 javac --module-path ".\lib\javafx\lib" --add-modules javafx.controls,javafx.fxml -d bin src/*.java src/board/*.java src/fxml/*.java src/player/*.java src/tile/*.java
 java --module-path ".\lib\javafx\lib" --add-modules javafx.controls,javafx.fxml -cp bin Main
+
+pause
